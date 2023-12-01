95a9ddbefdb495edbde2e6edd47445bd9d808869
srijan1805 <121806341+srijan1805@users.noreply.github.com>
2023-12-02 00:19:38 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center space-y-5 p-24">
-      <h1 className="text-2xl font-semibold">Movie Recommendation</h1>
+      <h1 className="text-2xl font-semibold text-center">Movie Recommendation</h1>
       <Link
         href={`/recommendations?q=${movieId}`}
         className="btn btn-primary uppercase"
